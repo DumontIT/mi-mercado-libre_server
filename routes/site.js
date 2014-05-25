6fcaabc841d7635b949e31ccb220887682dc7950
Nahuel Barrios <barrios.nahuel@gmail.com>
2014-05-25 11:30:14 -0300

    Refactor

--- routes/site.js
+++ routes/site.js
@@ -15,74 +15,63 @@ var findById = function (id, callback) {
 };
 
 exports.findAll = function (req, res, callback) {
+    var sendResponse = function (req, res, callback, sites) {
+        if (req && res) {
+            console.log('Obtained sites: ' + sites.length);
+            res.send(200, sites);
+        } else {
+            callback(error, sites);
+        }
+    };
+
     var getSitesBasicInfo = function (callback) {
         console.log('Getting sites from ML REST API...');
         meliObject.get('/sites', {}, callback);
     };
 
-    if (req && res) {
-        var afterGettingSitesBasicInfo = function (error, data) {
-            if (error) {
-                console.log('An error occurred while getting ML sites:' + error);
-                res.send(500, error);
-            } else {
-                console.log('Obtained sites: ' + data.length);
-                res.send(200, data);
-            }
-        };
+    var afterGettingSitesBasicInfo = function (error, sites) {
+        if (error) {
+            console.log('An error occurred while getting ML sites:' + error);
+            res.send(500, error);
+        } else {
 
-        Site.find(function (error, sites) {
-            if (error) {
-                console.log('An error occurred while getting ML sites from DB:' + error);
-                getSitesBasicInfo(afterGettingSitesBasicInfo);
-            } else if (sites.length > 0) {
-                console.log('Returning stored ML sites...');
-                res.send(200, sites);
-            } else {
-                console.log('There are NOT any site stored in DB');
-                getSitesBasicInfo(afterGettingSitesBasicInfo);
-            }
-        });
-    } else {
-        Site.find(function (error, sites) {
-            if (error) {
-                console.log('An error occurred while getting ML sites:' + error);
-                callback(error, sites);
-            } else {
-                if (sites.length === 0) {
-                    console.log('There no sites stored in DB, getting them from ML API...');
-                    getSitesBasicInfo(function (error, sites) {
+            var doForEachSite = function (eachSite, callback) {
+                findById(eachSite.id, callback);
+            };
+            var afterHavingAllSites = function (error, sites) {
+                var sitesForResponse = [];
 
-                        var doForEachSite = function (eachSite, callback) {
-                            findById(eachSite.id, callback);
-                        };
-                        var afterHavingAllSites = function (error, sites) {
-                            var sitesForResponse = [];
+                for (var i = 0; i < sites.length; i++) {
+                    var completeSite = sites[i];
+                    var reducedSite = {id: completeSite.id, name: completeSite.name, currencies: completeSite.currencies};
 
-                            for (var i = 0; i < sites.length; i++) {
-                                var completeSite = sites[i];
-                                var reducedSite = {id: completeSite.id, name: completeSite.name, currencies: completeSite.currencies};
+                    new Site(reducedSite).save();
+                    console.log('Saved site: ' + reducedSite.id + ', currencies: ' + reducedSite.currencies.length);
 
-                                new Site(reducedSite).save();
-                                console.log('Saved site: ' + reducedSite.id + ', currencies: ' + reducedSite.currencies.length);
-
-                                sitesForResponse.push(reducedSite);
-                            }
-
-                            callback(error, sitesForResponse);
-                        };
-
-                        var functions = sites.map(function (eachSite) {
-                            return doForEachSite.bind(undefined, eachSite);
-                        });
-
-                        async.parallel(functions, afterHavingAllSites);
-                    });
-                } else {
-                    console.log('There are stored sites in DB, returning...');
-                    callback(error, sites);
+                    sitesForResponse.push(reducedSite);
                 }
-            }
-        });
-    }
+
+                sendResponse(req, res, callback, sitesForResponse);
+            };
+
+            var functions = sites.map(function (eachSite) {
+                return doForEachSite.bind(undefined, eachSite);
+            });
+
+            async.parallel(functions, afterHavingAllSites);
+        }
+    };
+
+    Site.find(function (error, sites) {
+        if (error) {
+            console.log('An error occurred while getting ML sites from DB:' + error);
+            getSitesBasicInfo(afterGettingSitesBasicInfo);
+        } else if (sites.length > 0) {
+            console.log('Returning stored ML sites...');
+            sendResponse(req, res, callback, sites);
+        } else {
+            console.log('There are NOT any site stored in DB');
+            getSitesBasicInfo(afterGettingSitesBasicInfo);
+        }
+    });
 };
